5894302a4ba417fa6cb182ccdb6d112573a17367
KrivosheyYuriy <Lonespy@yandex.ru>
2023-03-20 19:57:50 +0300

    Beta-structure created

---
+++ data/register_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField, EmailField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    submit = SubmitField('Sign up')
--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
-
+from data import db_session
+from data.db_session import global_init
+from data.db_session import create_session
 from flask import Flask, redirect, render_template
+from flask_login import LoginManager
+from data.login_form import LoginForm
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    render_template('base.html')
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
+    render_template('main_page.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    from flask import render_template
-    return render_template('login.html', title='Авторизация', form=form)
+    # if form.validate_on_submit():
+    #     db_sess = db_session.create_session()
+    #     user = db_sess.query(User).filter(User.email == form.email.data).first()
+    #     if user and user.check_password(form.password.data):
+    #         login_user(user, remember=form.remember_me.data)
+    #         return redirect("/")
+    #     return render_template('login.html',
+    #                            message="Неправильный логин или пароль",
+    #                            form=form)
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
