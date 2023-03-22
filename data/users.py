9d67b8c678523c368ede7a8b933f8c9fecfc67aa
KrivosheyYuriy <Lonespy@yandex.ru>
2023-03-22 15:15:32 +0300

    Changed users.py

--- data/users.py
+++ data/users.py
@@ -11,11 +11,11 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     registration_data = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now())
 
-    def set_password(self):
+    def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
-    def check_password(self):
+    def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from flask import Flask, redirect, render_template, url_for
 from flask_login import LoginManager
 from data.login_form import LoginForm
 from data.register_form import RegisterForm
+from flask_login import login_user
 
 
 app = Flask(__name__)
@@ -25,15 +26,15 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    # if form.validate_on_submit():
-    #     db_sess = db_session.create_session()
-    #     user = db_sess.query(User).filter(User.email == form.email.data).first()
-    #     if user and user.check_password(form.password.data):
-    #         login_user(user, remember=form.remember_me.data)
-    #         return redirect("/")
-    #     return render_template('login.html',
-    #                            message="Неправильный логин или пароль",
-    #                            form=form)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
     return render_template('login.html', form=form)
 
 
